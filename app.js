ff5bac59a9cc04f61f01fc00751b87ded12cd1b4
itsrichardyu <38821021+itsrichardyu@users.noreply.github.com>
2020-07-11 12:30:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
+app.use(express.static("public"));
+
+// Serve /index.html when request to /
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// Serve /intro.html when request to /intro
+app.get("/intro", (request, response) => {
+  response.sendFile(__dirname + "/views/intro.html");
+});
+
+// Serve /play.html when request to /play
+app.get("/play", (request, response) => {
+  response.sendFile(__dirname + "/views/play.html");
+});
+
+// Serve /intro.html when request to /ending
+app.get("/ending", (request, response) => {
+  response.sendFile(__dirname + "/views/intro.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
